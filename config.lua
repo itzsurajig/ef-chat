cd61c42ee13126129f9f300fef0e2c179399e738
BlasterSuraj <surajsaraswat604@gmail.com>
2024-05-26 14:31:27 +0530

    optimized
    must update

--- config.lua
+++ config.lua
@@ -1,12 +1,1 @@
 Config = {}
-
--- [OOC]
-
-Config.EnableOOCCommand = true
-
-Config.OOCCommand = 'looc'
-
-Config.OOCDistance = 20.0
-
-
-Config.DateFormat = '%H:%M' -- To change the date format check this website - https://www.lua.org/pil/22.1.html
